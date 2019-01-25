9692854d22f324dfa5008eb8aa30fcff0f2df725
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2019-01-26 01:10:37 +0530

    Added Stats

--- MainController.js
+++ MainController.js
@@ -19,43 +19,50 @@ app.controller('MainController', function($scope) {
     });
 
     $('.usersubmit').click(() => {
+        
+        if($('#dist').val() && $('#speed').val())
+        {
+            // writing the logic for time dilation calculation
+            var distOption = document.getElementById('du');
+            if (distOption.options[distOption.selectedIndex].value === "MILES") {
+                $scope.dist = $('#dist').val() * 1.6093;
+            } else if (distOption.options[distOption.selectedIndex].value === "LIGHT YEARS") {
+                $scope.dist = $('#dist').val() * 9.46 * Math.pow(Math.E, 12);
+            } else if (distOption.options[distOption.selectedIndex].value === "ASTRONOMICAL UNITS") {
+                $scope.dist = $('#dist').val() * 149597871;
+            } else {
+                $scope.dist = $('#dist').val();
+            }
 
-        console.log(Math.E);
+            var speedOption = document.getElementById('su');
+            if (speedOption.options[speedOption.selectedIndex].value === "MILES/HOUR") {
+                $scope.speed = $('#speed').val() * 1.6093;
+            } else if (speedOption.options[speedOption.selectedIndex].value === "METRE/SEC") {
+                $scope.speed = $('#speed').val() * 3.6;
+            } else if (speedOption.options[speedOption.selectedIndex].value === "KM/SEC") {
+                $scope.speed = $('#speed').val() * 3600;
+            } else if (speedOption.options[speedOption.selectedIndex].value === "MILES/SEC") {
+                $scope.speed = $('#speed').val() * 1.6093 * 3600;
+            } else {
+                $scope.speed = $('#speed').val();
+            }
 
-        // writing the logic for time dilation calculation
-        var distOption = document.getElementById('du');
-        if (distOption.options[distOption.selectedIndex].value === "MILES") {
-            $scope.dist = $('#dist').val() * 1.6093;
-        } else if (distOption.options[distOption.selectedIndex].value === "LIGHT YEARS") {
-            $scope.dist = $('#dist').val() * 9.46 * Math.pow(Math.E, 12);
-        } else if (distOption.options[distOption.selectedIndex].value === "ASTRONOMICAL UNITS") {
-            $scope.dist = $('#dist').val() * 149597871;
-        } else {
-            $scope.dist = $('#dist').val();
+            $scope.timeOnEarth = $scope.dist / $scope.speed;
+            $scope.timeOnSpaceship = $scope.timeOnEarth * Math.sqrt(1 - (Math.pow($scope.speed, 2) / Math.pow($scope.speedOfLight, 2)));
+            $scope.dilatedDistance = $scope.speed * $scope.timeOnSpaceship;
+            $scope.ageDifference = (2 * $scope.timeOnEarth) - (2 * $scope.timeOnSpaceship);
+
+            console.log('Time Elapsed On Earth - ' + $scope.timeOnEarth);
+            console.log('Time Elapsed On SpaceShip - ' + $scope.timeOnSpaceship);
+
+            $('.userzone').slideUp(1000);
+            $('.userresult').slideDown(1000);
         }
-
-        var speedOption = document.getElementById('su');
-        if (speedOption.options[speedOption.selectedIndex].value === "MILES/HOUR") {
-            $scope.speed = $('#speed').val() * 1.6093;
-        } else if (speedOption.options[speedOption.selectedIndex].value === "METRE/SEC") {
-            $scope.speed = $('#speed').val() * 3.6;
-        } else if (speedOption.options[speedOption.selectedIndex].value === "KM/SEC") {
-            $scope.speed = $('#speed').val() * 3600;
-        } else if (speedOption.options[speedOption.selectedIndex].value === "MILES/SEC") {
-            $scope.speed = $('#speed').val() * 1.6093 * 3600;
-        } else {
-            $scope.speed = $('#speed').val();
+        else if ($('#dist').val() == 0) {
+            
         }
-
-        $scope.timeOnEarth = $scope.dist / $scope.speed;
-        $scope.timeOnSpaceship = $scope.timeOnEarth * Math.sqrt(1 - (Math.pow($scope.speed, 2) / Math.pow($scope.speedOfLight, 2)));
-        $scope.dilatedDistance = $scope.speed * $scope.timeOnSpaceship;
-        $scope.ageDifference = (2 * $scope.timeOnEarth) - (2 * $scope.timeOnSpaceship);
-
-        console.log('Time Elapsed On Earth - ' + $scope.timeOnEarth);
-        console.log('Time Elapsed On SpaceShip - ' + $scope.timeOnSpaceship);
-
-        $('.userzone').slideUp(1000);
-        $('.userresult').slideDown(1000);
+        else if ($('#speed').val() == 0) {
+            
+        }
     });
 });
